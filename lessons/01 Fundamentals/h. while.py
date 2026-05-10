d7fb598b2b8191fdca7e4c863f39c2af8ba78500
Kaung Pyae Htet <kaungpyaehtet2007@gmail.com>
2026-05-10 20:01:44 +0630

    10/05/2026

---
+++ lessons/01 Fundamentals/h. while.py
@@ -0,0 +1,5 @@
+number = int(input("Enter a number:"))
+
+while number > 100:
+    print("Your number is greater than 100")
+    number = int(input("Enter a number:"))
---
+++ lessons/01 Fundamentals/i. list.py
@@ -0,0 +1,17 @@
+studentNames = [
+    "Ozzy", # 0 index
+    "Nora", # 1 index
+    "Htet", # 2 index
+    "Hello" # 3 index
+]
+
+print(studentNames)
+studentNames.append("One more student")
+
+# Get the student by index
+print(studentNames[3])
+
+# Change the element inside string
+studentNames[0] = "Kaung Pyae Htet"
+
+print(studentNames)
